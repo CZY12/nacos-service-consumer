a083a550ca0425e0ddc4455459b0670d32641236
Czy <czy610175932@qq.com>
2023-09-10 16:46:13 +0800

    初始化

---
+++ src/test/java/com/example/nacosserviceconsumer/NacosServiceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nacosserviceconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosServiceConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
